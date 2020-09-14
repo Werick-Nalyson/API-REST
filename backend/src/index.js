3e04b892d2074326f3b3d51390a31463caa66841
Werick-Nalyson <46249623+Werick-Nalyson@users.noreply.github.com>
2020-09-14 00:18:00 -0300

    Adicionado middleware de autenticação com JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
+
+const JWTsecret = "senhaminhaadmin";
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -27,13 +30,83 @@ let DB = {
             year: 2018,
             price: 40
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Victor Lima",
+            email: "victorlima@gmail.com",
+            password: "nodejs"
+        },
+        {
+            id: 2,
+            name: "Robert klaus",
+            email: "robertklaus@gmail.com",
+            password: "nodejs"
+        },
+        {
+            id: 3,
+            name: "Werick Nalyson",
+            email: "wericknalyson@gmail.com",
+            password: "nodejs"
+        },
     ]
 }
 
-app.get('/games', (request, response) => {
+function auth(request, response, next) {
+    const authToken = request.headers['authorization'];
+    
+    if (authToken != undefined) {
+        const bearer = authToken.split(' ');
+        let token = bearer[1];
+        
+        jwt.verify(token, JWTsecret, (err, data) => {
+            if (err) {
+                response.status(401).json({err: "Token inválido!"})
+            } else {
+                request.token = token;
+                request.loggedUser = { id: data.id, email: data.email }
+                next();
+            }
+        });
+
+    } else {
+        response.status(401).json({err: "Token inválido!"})
+    }
+
+}
+
+app.post('/auth', (request, response) => {
+    const { email, password } = request.body;
+
+    if (email != undefined) {
+        const user = DB.users.find(user => user.email == email);
+
+        if (user != undefined) {
+            if (user.password == password) {
+
+                jwt.sign({ id: user.id, email: user.email }, JWTsecret, {expiresIn: '48h'}, (err, token) => {
+                    if (err) {
+                        return response.status(400).json({ err: "Falha interna" });        
+                    } else {
+                        return response.status(200).json({ token: token });
+                    }
+                });
+            } else {
+                return response.status(404).json({ err: "Credenciais inválidas" });
+            }
+        } else {
+            return response.status(404).json({ err: "O E-mail enviado não existe na base de dados" });
+        }
+    } else {
+        return response.status(404).json({ err: "O E-mail enviado é inválido" });
+    }
+});
+
+app.get('/games', auth, (request, response) => {
     return response
         .status(200)
-        .json(DB.games);
+        .json({ user: request.loggedUser, games: DB.games });
 });
 
 app.get('/games/:id', (request, response) => {
